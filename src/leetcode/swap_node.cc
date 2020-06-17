9120a8ba4c54d901a483f2fdf5262511247d3af0
wego <zhangweiguo717@163.com>
2020-06-17 16:23:31 +0800

    reinit

---
+++ src/leetcode/swap_node.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *n0, *n1;
+        
+    }
+};
+
+int main() {
+
+}
