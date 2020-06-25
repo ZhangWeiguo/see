21273be801c6d9a37d83874014fef0cc6b65bafd
zhangweiguo <zhangweiguo717@163.com>
2020-06-25 18:53:20 +0800

    add flags and log

--- src/test/test_grpc_client.cc
+++ src/test/test_grpc_client.cc
@@ -1,36 +1,45 @@
+#include <stdio.h>
+
 #include <iostream>
 #include <string>
-#include "grpc/grpc.h"
-#include "grpc++/grpc++.h"
-#include "grpc++/channel.h"
 
-#include "test/proto/helloworld.pb.h"
+#include "gflags/gflags.h"
+#include "grpc++/channel.h"
+#include "grpc++/grpc++.h"
+#include "grpc/grpc.h"
 #include "test/proto/helloworld.grpc.pb.h"
+#include "test/proto/helloworld.pb.h"
+
 
 using namespace helloworld;
 
+DEFINE_string(grpc_port, "8080", "GRPC PORT");
+
 class GreeterClient {
-public:
-  GreeterClient(std::shared_ptr<grpc::Channel> channel): stub_(Greeter::NewStub(channel))  { }
-	void SayHello(std::string message) {
-		grpc::ClientContext context;
+ public:
+  GreeterClient(std::shared_ptr<grpc::Channel> channel)
+      : stub_(Greeter::NewStub(channel)) {}
+  void SayHello(std::string message) {
+    grpc::ClientContext context;
     HelloRequest request;
     request.set_message(message);
     HelloReply response;
-	  if (stub_->SayHello(&context, request, &response).ok()) {
-			std::cout << "succ request = " << request.message() << ", response = " << response.message() << std::endl;
-		} else {
+    if (stub_->SayHello(&context, request, &response).ok()) {
+      std::cout << "succ request = " << request.message()
+                << ", response = " << response.message() << std::endl;
+    } else {
       std::cout << "failed!";
     }
-	}
-private:
-	std::unique_ptr<Greeter::Stub> stub_;
+  }
+
+ private:
+  std::unique_ptr<Greeter::Stub> stub_;
 };
 
-
 int main(int argc, char **argv) {
-	auto channel = grpc::CreateChannel("localhost:5000", grpc::InsecureChannelCredentials());
-	GreeterClient client(channel);
-	client.SayHello("I am wego");
-	return 0;
+  std::string port = "localhost:" + FLAGS_grpc_port;
+  auto channel = grpc::CreateChannel(port, grpc::InsecureChannelCredentials());
+  GreeterClient client(channel);
+  client.SayHello("I am wego");
+  return 0;
 }
--- src/test/test_grpc_server.cc
+++ src/test/test_grpc_server.cc
@@ -1,11 +1,11 @@
-#include <grpc++/grpc++.h>
-#include <grpc++/server.h>
-#include <grpc++/server_builder.h>
-#include <grpc++/server_context.h>
-#include <grpc/grpc.h>
-
 #include <iostream>
 
+#include "gflags/gflags.h"
+#include "grpc++/grpc++.h"
+#include "grpc++/server.h"
+#include "grpc++/server_builder.h"
+#include "grpc++/server_context.h"
+#include "grpc/grpc.h"
 #include "test/proto/helloworld.grpc.pb.h"
 
 
@@ -13,7 +13,7 @@ using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-
+DEFINE_string(grpc_port, "8080", "GRPC PORT");
 class SearchRequestImpl final : public helloworld::Greeter::Service {
   ::grpc::Status SayHello(::grpc::ServerContext* context,
                           const ::helloworld::HelloRequest* request,
@@ -24,7 +24,7 @@ class SearchRequestImpl final : public helloworld::Greeter::Service {
 };
 
 void RunServer() {
-  std::string server_address("0.0.0.0:8080");
+  std::string server_address("0.0.0.0:" + FLAGS_grpc_port);
   SearchRequestImpl service;
   ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
